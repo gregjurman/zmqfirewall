bf1243c49e0db3749558ec16e8377c1b9c880160
Greg Jurman <gdj2214@rit.edu>
2012-02-25 16:46:08 -0500

    Removed action handler in message. This may get reverted in the future

--- zmqfirewall/message.py
+++ zmqfirewall/message.py
@@ -1,16 +1,17 @@
 import actions
 
 class Message(object):
-    def __init__(self, from_host, message, topic=None, def_action=actions.pass_on):
+    def __init__(self, message, from_host, topic=None):
         self.from_host = from_host
         self.topic = topic
         self.message = message
         self.topic = topic
-        self.default_action = action
 
     def __repr__(self):
-        return "<Message: from=%s, topic=%s, action=%s>" % (
+        return "<Message: from=%s, topic=%s>" % (
             self.from_host,
             self.topic,
-            self.action,
         )
+
+    def __str__(self):
+        return self.message
