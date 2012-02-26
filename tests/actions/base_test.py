65c7f86c2ddeb406c20adcf10c1144d59d711c9f
Greg Jurman <gdj2214@rit.edu>
2012-02-25 19:57:21 -0500

    Added test to check for duplicate actions in index

--- tests/actions/base_test.py
+++ tests/actions/base_test.py
@@ -3,24 +3,41 @@ import zmqfirewall.actions
 from nose.tools import eq_
 
 def test_good_action():
-    class TestAction(zmqfirewall.actions.Action):
+    class GoodActionTestAction(zmqfirewall.actions.Action):
         def action(self, message):
             return message
 
 def test_missing_action():
     try:
-        class MissingTestAction(zmqfirewall.actions.Action):
+        class MissingActionTestAction(zmqfirewall.actions.Action):
             pass
 
         assert(False)
     except AttributeError, e:
-        eq_(str(e), 'MissingTestAction is missing an action callback')
+        eq_(str(e), 'MissingActionTestAction is missing an action callback')
 
 def test_bad_action():
     try:
-        class BadTestAction(zmqfirewall.actions.Action):
+        class BadActionTestAction(zmqfirewall.actions.Action):
             action = "I am not callable"
 
         assert(False)
     except AttributeError, e:
-        eq_(str(e), 'BadTestAction is missing an action callback')
+        eq_(str(e), 'BadActionTestAction is missing an action callback')
+
+
+def test_duplicate_action():
+    try:
+        class WorkingActionTestAction(zmqfirewall.actions.Action):
+            def action(self, message):
+                return message
+
+        class IdiotlyNamedAction(zmqfirewall.actions.Action):
+            name = 'workingactiontest'
+
+            def action(self, message):
+                pass
+
+        assert(False)
+    except NameError as e:
+        eq_(str(e),"An action named 'workingactiontest' is already registered!")
