41032de5e44026f0ee18449d108f11fb2f30d114
Greg Jurman <gdj2214@rit.edu>
2012-02-28 04:05:46 -0500

    deffered import to remove circular import

--- utils/lookup.py
+++ utils/lookup.py
@@ -1,10 +1,9 @@
-from moksha.firewall.actions.base import ActionMeta
-from moksha.firewall.filters.base import FilterMeta
-
 __all__ = ['get_action', 'get_filter']
 
 def get_action(name):
+    from moksha.firewall.actions.base import ActionMeta
     return ActionMeta.get_action_by_name(name)
 
 def get_filter(name):
+    from moksha.firewall.filters.base import FilterMeta
     return FilterMeta.get_filter_by_name(name)
