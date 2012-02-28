9e225241065d121f6e068a70a6b06d7475d4c073
Greg Jurman <gdj2214@rit.edu>
2012-02-28 18:16:50 -0500

    Additional function tests

--- tests/functional/functional_tests.py
+++ tests/functional/functional_tests.py
@@ -19,12 +19,13 @@ class AppendMD5Action(Action):
     no_register = True
     def action(self, msg):
         msg.body = "%s [%s]" % (msg.body, md5(msg.body).hexdigest())
+        return msg
 
 class MarkAsImportantLogMessage(Action):
     no_register = True
     def action(self, msg):
         msg.body = "[IMPORTANT] %s" % msg.body
-        return
+        return msg
 
 class ImportantMessageAction(Action):
     no_register = True
@@ -51,6 +52,13 @@ class FunctionalFilterTestFilter(FirewallFilter):
 
 def bad_message_test():
     msg = Message(body="i am a bad message", topic="not_log", from_host="localhost")
-
     out = FunctionalFilterTestFilter.handle(msg)
     eq_(str(out), "[BAD MESSAGE] i am a bad message")
+
+    msg = Message(body="i am an important log!", topic="log_message", from_host="localhost")
+    out = FunctionalFilterTestFilter.handle(msg)
+    eq_(str(out), "[IMPORTANT] i am an important log! [04f6255dd0c703fc555e7605e3e1df73]")
+
+    msg = Message(body="i am a regular log!", topic="log_message", from_host="localhost")
+    out = FunctionalFilterTestFilter.handle(msg)
+    eq_(str(out), "i am a regular log! [129d357ed288b7c6bf119e76033689a1]")
