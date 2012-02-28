e94a345b4e2df58c25f02a9a364805b6d307afc2
Greg Jurman <gdj2214@rit.edu>
2012-02-28 04:07:51 -0500

    namespace correction

--- zmqfirewall/utils/lookup.py
+++ zmqfirewall/utils/lookup.py
@@ -1,9 +1,9 @@
 __all__ = ['get_action', 'get_filter']
 
 def get_action(name):
-    from moksha.firewall.actions.base import ActionMeta
+    from zmqfirewall.actions.base import ActionMeta
     return ActionMeta.get_action_by_name(name)
 
 def get_filter(name):
-    from moksha.firewall.filters.base import FilterMeta
+    from zmqfirewall.filters.base import FilterMeta
     return FilterMeta.get_filter_by_name(name)
