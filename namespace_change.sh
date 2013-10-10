1ea80e436273116280ee763f37eff639bbc49609
Greg Jurman <jurman.greg@gmail.com>
2013-10-10 16:59:43 -0400

    Remove old moksha namespace hack script

--- namespace_change.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# What are you doing gitignore, youre not a normal file
-rm zmqfirewall/.gitignore
-
-while read -r line; do
-    sed 's/moksha.firewall/zmqfirewall/g' -i $line
-done <<< "`grep -lir "moksha" zmqfirewall`"
-
